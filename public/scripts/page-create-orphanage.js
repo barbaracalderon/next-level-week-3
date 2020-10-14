d33adc9897d60217c42c2e4186b70f5a34d6c7f7
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-14 13:34:48 -0300

    Added format to documents

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,87 +1,85 @@
 // create map
-const map = L.map('mapid').setView([-27.5926317,-48.5287365], 15);
+const map = L.map("mapid").setView([-27.5926317, -48.5287365], 15);
 
 // create and add tileLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-})
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+});
 
 let marker;
 
 // create and add marker
-map.on('click', (event) => {
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
+map.on("click", (event) => {
+  const lat = event.latlng.lat;
+  const lng = event.latlng.lng;
 
-    document.querySelector('[name=lat]').value = lat;
-    document.querySelector('[name=lng]').value = lng;
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
-    // remove icon
-    marker && map.removeLayer(marker)
+  // remove icon
+  marker && map.removeLayer(marker);
 
-    // add icon layer
-    marker = L.marker([lat, lng], { icon })
-    .addTo(map)
-
-})
+  // add icon layer
+  marker = L.marker([lat, lng], { icon }).addTo(map);
+});
 
 // adicionar o campo de fotos
 function addPhotoField() {
-    // pegar o container de fotos #images
-    const container = document.querySelector('#images')
-    // pegar o container para duplicar .new-upload
-    const fieldsContainer = document.querySelectorAll('.new-upload')
-    // realizar o clone da última imagem adicionada.
-    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
-    //verificar se o campo está vazio, se sim, não adicionar ao container de imagens
-    const input = newFieldContainer.children[0]
+  // pegar o container de fotos #images
+  const container = document.querySelector("#images");
+  // pegar o container para duplicar .new-upload
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+  // realizar o clone da última imagem adicionada.
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
+  //verificar se o campo está vazio, se sim, não adicionar ao container de imagens
+  const input = newFieldContainer.children[0];
 
-    if(input.value == "") {
-        return
-    }
+  if (input.value == "") {
+    return;
+  }
 
-    // limpar o campo antes de adicionar ao container de imagens
-    input.value = ""
+  // limpar o campo antes de adicionar ao container de imagens
+  input.value = "";
 
-    // adicionar o clone ao container de #images
-    container.appendChild(newFieldContainer)
+  // adicionar o clone ao container de #images
+  container.appendChild(newFieldContainer);
 }
 
 function deleteField(event) {
-    const span = event.currentTarget
+  const span = event.currentTarget;
 
-    const fieldsContainer = document.querySelectorAll('.new-upload')
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    if(fieldsContainer.length < 2) {
-        // limpar o valor do campo
-        span.parentNode.children[0].value = ""
-        return 
-    }
+  if (fieldsContainer.length < 2) {
+    // limpar o valor do campo
+    span.parentNode.children[0].value = "";
+    return;
+  }
 
-    // deletar o campo
-    span.parentNode.remove()
+  // deletar o campo
+  span.parentNode.remove();
 }
 
 // select yes or no
 function toggleSelect(event) {
-    // retirar a class .active dos botões
-    document.querySelectorAll('.button-select button')
-    .forEach( function(button) {
-        button.classList.remove('active')
-    })
+  // retirar a class .active dos botões
+  document.querySelectorAll(".button-select button").forEach(function (button) {
+    button.classList.remove("active");
+  });
 
-    // colocar a class .active nesse botão clicado
-    const button = event.currentTarget
-    button.classList.add('active')
+  // colocar a class .active nesse botão clicado
+  const button = event.currentTarget;
+  button.classList.add("active");
 
-    // atualizar o meu input hidden com o valor selecionado
-    const input = document.querySelector('[name="open_on_weekends"]')
+  // atualizar o meu input hidden com o valor selecionado
+  const input = document.querySelector('[name="open_on_weekends"]');
 
-    input.value = button.dataset.value
+  input.value = button.dataset.value;
 }
-
