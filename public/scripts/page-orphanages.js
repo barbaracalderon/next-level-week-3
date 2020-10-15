2532e8a719157fa57137f8ca76d706cbbe87061e
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-15 17:06:37 -0300

    Added some back-end (4th day)

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -6,21 +6,37 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 })
 
-// create popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+function addMarker({id, name, lat, lng}) {
 
-// create and  add marker
-L.marker([-27.5926317,-48.5287365], {icon}).addTo(map)
-    .bindPopup(popup)
-    .openPopup();
+
+    // create popup overlay
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`)
+
+    // create and  add marker
+    L.marker([lat, lng], { icon })
+        .addTo(map)
+        .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+    }
+
+    addMarker(orphanage)
+})
